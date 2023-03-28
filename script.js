5c947343eaf4d943685d80d04437e8a59313a80e
Piotr Ochodek <ohis@op.pl>
2023-03-28 09:06:20 +0200

    I added an if condition to check if the entered parameter is of type number

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 // a2 + b2 = c2
 const isRectangularTriangle = (x1, x2, x3) => {
-  const nums = [x1, x2, x3].sort((a, b) => {
+  if (typeof x1 !== 'number') {
+    console.log('Input is not a number');
+    return;
+  } else if (typeof x2 !== 'number') {
+    console.log('Input is not a number');
+    return;
+  } else if (typeof x3 !== 'number') {
+    console.log('Input is not a number');
+    return;
+  }
+  const numbers = [x1, x2, x3].sort((a, b) => {
     return a - b;
   });
 
-  const a2 = nums[0];
-  const b2 = nums[1];
-  const c2 = nums[2]
+  const a2 = numbers[0];
+  const b2 = numbers[1];
+  const c2 = numbers[2];
 
   if (Math.pow(a2, 2) + Math.pow(b2, 2) === Math.pow(c2, 2)) {
-    true
-    console.log(`true`)
+    console.log(`true`);
+    return true;
   } else {
-    false
-    console.log(`From the given lengths of the segments it is impossible to build a right triangle`);
+    console.log(
+      `From the given lengths of the segments it is impossible to build a right triangle`
+    );
+    return false;
   }
 };
 
 isRectangularTriangle(6, 8, 10);
+isRectangularTriangle(6, 8, '10');
